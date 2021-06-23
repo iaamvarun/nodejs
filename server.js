ac3f6d08ca8cf0a40a67e6b6b972f77b5736f288
iaamvarun <85957398+iaamvarun@users.noreply.github.com>
2021-06-24 01:27:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
   res.send('<h1 style="color:green;">Java Home App - version-10!!</h1> \n');
 });
 
-app.listen(PORT, HO);
+app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
